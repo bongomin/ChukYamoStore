4b4c3d8fbacbfee64a2a7d82e13ba491e28672b2
Bongomin Daniel <bongomindaniel@gmail.com>
2023-11-18 14:26:45 +0300

    config static,media files

--- ChukYamoStoreProject/settings.py
+++ ChukYamoStoreProject/settings.py
@@ -124,8 +124,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# Media Files Configurations
 MEDIA_URL = '/media/'  # Corrected typo here
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# # Media Files Configurations
+# MEDIA_URL = '/media'
+# MEDIA_ROOT
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
