7f582fed5bd4f54dbe8cbcae23378530b9e32c26
Bongomin Daniel <bongomindaniel@gmail.com>
2023-11-20 16:27:36 +0300

    add category filter

--- store/views.py
+++ store/views.py
@@ -1,13 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Product
+from category.models import Category
 
-# Create your views here.
-def store(request):
-    products = Product.objects.all().filter(is_available=True)
-    product_counts = products.count()
+def store(request, category_slug=None):
+    categories = None
+    products = None
+
+    if category_slug:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.all().filter(category=categories, is_available=True)
+        product_counts = products.count()
+    else:
+        products = Product.objects.all().filter(is_available=True)
+        product_counts = products.count()
+
     context = {
-        'products' : products,
-        'product_counts': product_counts
+        'products': products,
+        'product_counts': product_counts,
     }
 
     return render(request, 'store/store.html', context)
+
+
